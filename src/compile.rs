b01882efb2395095097c48b1f3cd911b2fb3b018
Cashigtro <cashignitro@gmail.com>
2023-12-08 14:24:48 +1100

    trying to fix printing text to the console

--- src/compile.rs
+++ src/compile.rs
@@ -1,4 +1,4 @@
-use std::process::Command;
+use std::{process::Command, process::Stdio, io::Write};
 use crate::ArgumentsStruct;
 use std::env::consts::OS;
 
@@ -10,16 +10,24 @@ fn get_os() -> &'static str {
 }
 
 pub fn compile(settings: ArgumentsStruct) {
+    std::io::stdout().flush().expect("Couldnt not");
     let current_os = get_os();
     println!("{}", settings.file_extension);
     if current_os == "linux" {
         if settings.file_extension == "cpp"{
-            let gpp = Command::new("g++")
-                .arg(settings.source_path)
-                .spawn()
-                .expect("Failed to execute gcc");
+            print!("\n");
+            if settings.build == true {
+                let gpp = Command::new("g++")
+                    .arg(settings.source_path)
+                    .stdout(Stdio::piped())
+                    .spawn()
+                    .expect("Failed to execute gcc");
+            }
             if settings.run == true {
-                let run = Command::new("./a.out").spawn();
+                std::io::stdout().flush().expect("Couldnt not");
+                let run = Command::new("./a.out")
+                    .stdout(Stdio::piped())
+                    .spawn();
             }
             // println!("stdout: {}", String::from_utf8_lossy(&gcc.stderr))
         }
