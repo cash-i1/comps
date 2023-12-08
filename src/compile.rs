3d10dfcbf18bdf23116a597ea29bc97636d15340
Cashigtro <cashignitro@gmail.com>
2023-12-08 13:08:03 +1100

    Added build and run commands

--- src/compile.rs
+++ src/compile.rs
@@ -1,6 +1,6 @@
 use std::process::Command;
-use std
+// use std
 
 pub fn compile() {
-    
+    println!("test");
 }
--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,47 @@
-use clap::{command, Arg};
+use clap::{command, Arg, ArgMatches, ArgAction};
 mod compile;
 
-fn main() {
-    let command = command!()
+struct ArgumentsStruct {
+    run: bool,
+    build: bool,
+}
+
+fn get_matches() -> ArgMatches {
+    return command!()
         .arg(
             Arg::new("build")
                 .num_args(0)
                 .short('b')
                 .long("build")
+                .action(clap::ArgAction::SetTrue)
         )
+        .arg(
+            Arg::new("run")
+                .num_args(0)
+                .short('r')
+                .long("run")
+                .action(clap::ArgAction::SetTrue)
+                // .requires("build")
+
+        )
+
+
         .get_matches();
+}
 
-        if command.contains_id("build") {
-            println!("Building");
-            compile::compile();
-        } else {
-            println!("else.");
+fn main() {
+        let command = get_matches();
+
+        let settings = ArgumentsStruct {
+            run: command.get_flag("run"),
+            build: command.get_flag("build")
+        };
+            
+
+        if settings.run {
+            println!("Runn is turee")
         }
-
+        if settings.build {
+            println!("build is turee")
+        }
 }
